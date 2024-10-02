39a97f16401d2ad54c0fa0ffcfe110fa6c5a880d
christopher-mei <chixelpuf@gmail.com>
2024-10-02 11:50:24 -0400

    testing end point that returns allusers

--- controllers/userController.js
+++ controllers/userController.js
@@ -92,4 +92,14 @@ userRouter.put('/update', authenticate, async (req, res) => {
   }
 });
 
+// Get all users (protected)
+userRouter.get('/users', authenticate, async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = userRouter;
